ed479d620b074d49e9d7c11f99e4e665c02acd64
Ayush Dixit <dixitayush450@gmail.com>
2026-09-23 20:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.60%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,29 +1,65 @@
 class Solution {
 public:
+
+    // k-th node find karne ke liye helper function
+    ListNode* findNthNode(ListNode* temp, int k) {
+
+        int cnt = 1;
+
+        while (temp != NULL) {
+
+            // Agar required position mil gayi
+            if (cnt == k)
+                return temp;
+
+            cnt++;
+            temp = temp->next;
+        }
+
+        return temp;
+    }
+
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL|| head->next==NULL|| k==0) return head;
-        ListNode*slow=head;
-        ListNode*fast=head;
-        int n=0;
-        ListNode*temp=head;
-        while(temp!=NULL)
-        {
-            n++;
-            temp=temp->next;
+
+        // Agar list empty hai ya k = 0 hai
+        // toh rotation ki zarurat nahi hai
+        if (head == NULL || k == 0)
+            return head;
+
+        // Tail find karenge
+        ListNode* tail = head;
+
+        // Length initially 1
+        int len = 1;
+
+        // List ke end tak jao
+        while (tail->next != NULL) {
+            tail = tail->next;
+            len++;
         }
-        k=k%n;
-        for(int i=0;i<k;i++)
-        {
-            fast=fast->next;
-        }
-        while(fast->next!=NULL)
-        {
-            slow=slow->next;
-            fast=fast->next;
-        }
-        fast->next=head;
-        head=slow->next;
-        slow->next=NULL;
+
+        // Agar k list ki length se bada hai,
+        // toh unnecessary rotations remove kar do
+        k = k % len;
+
+        // Agar k = 0 ho gaya,
+        // toh list same rahegi
+        if (k == 0)
+            return head;
+
+        // Tail ko head se connect karke
+        // circular linked list bana do
+        tail->next = head;
+
+        // New tail ki position = len - k
+        ListNode* newLastNode = findNthNode(head, len - k);
+
+        // New head new tail ke next par hoga
+        head = newLastNode->next;
+
+        // Circular connection tod do
+        newLastNode->next = NULL;
+
         return head;
     }
 };
